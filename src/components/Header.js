84328a74f55cff98f8751b9d79ed1a69e3965f51
Duy Anh <ndanhdev@gmail.com>
2023-06-15 13:45:34 +0700

    #23

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,16 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import logoApp from '../assets/images/logo-small.png';
-import { useLocation, NavLink } from 'react-router-dom';
+import { useLocation, NavLink, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const Header = (props) => {
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        navigate('/');
+        toast.success('Log out success!');
+    };
     const location = useLocation();
     return (
         <div>
@@ -31,9 +38,11 @@ const Header = (props) => {
                             </NavLink>
                         </Nav>
                         <Nav>
-                            <NavDropdown title="Setting" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="/login">Login</NavDropdown.Item>
-                                <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+                            <NavDropdown title="Login/Logout" id="basic-nav-dropdown">
+                                <NavLink to="/login" className="dropdown-item">
+                                    Login
+                                </NavLink>
+                                <NavDropdown.Item onClick={() => handleLogout()}>Logout</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,45 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { loginApi } from '~/services/UserService';
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
+    const navigate = useNavigate();
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isShowPassword, setIsShowPassword] = useState(false);
+    const [loadingAPI, setLoadingAPI] = useState(false);
+
+    useEffect(() => {
+        let token = localStorage.getItem('token');
+        if (token) {
+            navigate('/');
+        }
+    }, []);
+
     const handleLogin = async () => {
-        alert('ME');
         if (!email || !password) {
             toast.error('Email/Password is required');
             return;
         }
-        let res = await loginApi('eve.holt@reqres.in', password);
+        setLoadingAPI(true);
+        let res = await loginApi(email, password);
         if (res && res.token) {
             localStorage.setItem('token', res.token);
+            navigate('/');
+        } else {
+            // error
+            if (res && res.status === 400) {
+                toast.error(res.data.error);
+            }
         }
-        console.log('>>> Check login: ', res);
+        setLoadingAPI(false);
     };
     return (
         <>
             <div className="login-container col-12 col-sm-4">
                 <div className="title">Log in</div>
-                <div className="text">Email or username</div>
+                <div className="text">Email or username ( eve.holt@reqres.in )</div>
                 <input
                     type="text"
                     placeholder="Email or username..."
@@ -45,7 +63,7 @@ const Login = () => {
                     disabled={email && password ? false : true}
                     onClick={() => handleLogin()}
                 >
-                    Login
+                    {loadingAPI && <i className="fas fa-sync fa-spin"></i>} &nbsp;Login
                 </button>
                 <div className="back">
                     <i className="fa-solid fa-angles-left"></i> Go back
